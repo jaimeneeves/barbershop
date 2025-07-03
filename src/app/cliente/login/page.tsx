0c9ffa58892364ce581e6296a9472f62b72ca38a
Jaime <jaimebarrosoneves@gmail.com>
2025-07-03 09:40:18 -0300

    feat: enhance login component layout and styling for improved user experience

--- src/app/cliente/login/page.tsx
+++ src/app/cliente/login/page.tsx
@@ -3,8 +3,6 @@ import LoginCliente from "@/components/cliente/LoginCliente";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen items-center justify-center px-4">
-      <LoginCliente />
-    </div>
+    <LoginCliente />
   );
 }
